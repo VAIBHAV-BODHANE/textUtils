589fb07a7a3857660b23e4b4b49e71ba1a710cd1
APEXDEAD <vbodhane29@gmail.com>
2019-10-13 13:08:23 +0530

    django 2.2.6

---
+++ textutils/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def analyze(request):
+    textBox = request.POST.get('text', 'default')
+    print(textBox)
+    removepunc = request.POST.get('removepunc', 'off')
+    uppercase = request.POST.get('uppercase', 'off')
+    lineremover = request.POST.get('lineremover', 'off')
+    spaceremover = request.POST.get('spaceremover', 'off')
+
+    if removepunc == 'on':
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ''
+        for char in textBox:
+            if char not in punctuations:
+                analyzed = analyzed + char
+
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        textBox = analyzed
+
+    if uppercase == 'on':
+        analyzed = ''
+        for char in textBox:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        textBox = analyzed
+
+    if lineremover == 'on':
+        analyzed = ''
+        for char in textBox:
+            if char != "\n" and char != "\r":
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        textBox = analyzed
+
+    if spaceremover == 'on':
+        analyzed = ""
+        for index, char in enumerate(textBox):
+            if not (textBox[index] == " " and textBox[index + 1] == " "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+
+    if ( removepunc != "on" and uppercase != "on" and lineremover != "on" and spaceremover != "on" ):
+        return HttpResponse("Please select any operation and try again!")
+
+    return render(request, 'analyze.html', params)
+
+
